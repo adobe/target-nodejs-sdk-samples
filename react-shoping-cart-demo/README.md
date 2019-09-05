f2b9b400dab5286d188fa6e68e6451851946699e
Andrei Anischevici <anischev@adobe.com>
2019-09-05 17:36:06 +0300

    Rename react and ssr demo dirs

--- react-shoping-cart-demo/README.md
+++ react-shoping-cart-demo/README.md
@@ -1,8 +1,12 @@
 # Target React shopping cart sample
 
+# Overview
+
 A shopping cart sample using the full potential Target Node Client SDK by prefetching the content for React app views
 on the server-side and injecting these in the content returned on first page load, thus enabling instant application
 of Target offers on the client side, without any additional Target calls initiated by Target client at.js library. 
+
+
 
 ## Usage
 1. Install dependencies: `npm i`
--- react-shoping-cart-demo/server.js
+++ react-shoping-cart-demo/server.js
@@ -1,21 +1,32 @@
 const fs = require("fs");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+// Load the Target NodeJS SDK library
 // const TargetNodeClient = require("@adobe/target-node-client");
 const TargetNodeClient = require("./tmp-node-client/index"); // Temp fix until NodeJS SDK is public
-
-const TEMPLATE = fs.readFileSync(`${__dirname}/index.tpl`).toString();
+// Load the Target configuration (replace with configurations specific to your Adobe Client & Org)
 const CONFIG = require("./config.json");
+// Load the template of the HTML page returned in the response
+const TEMPLATE = fs.readFileSync(`${__dirname}/index.tpl`).toString();
 
+// Initialize the Express app
 const app = express();
 const PORT = process.env.PORT;
 
+// Enable TargetNodeClient logging via the console logger
 const targetOptions = Object.assign({ logger: console }, CONFIG);
+// Create the TargetNodeClient global instance
 const targetClient = TargetNodeClient.create(targetOptions);
 
+// Setup cookie parsing middleware and static file serving from the /public directory
 app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
 
+/**
+ * Sets cookies in the response object
+ * @param res response to be returned to the client
+ * @param cookie cookie to be set
+ */
 function saveCookie(res, cookie) {
   if (!cookie) {
     return;
@@ -24,15 +35,22 @@ function saveCookie(res, cookie) {
   res.cookie(cookie.name, cookie.value, { maxAge: cookie.maxAge * 1000 });
 }
 
-function sendHtml(res, response) {
+/**
+ * Augments the HTML template with Target data and sends it to the client
+ * @param res response object returned to the client
+ * @param targetResponse response received from Target Delivery API
+ */
+function sendHtml(res, targetResponse) {
+  // Set the serverState object, which at.js will use on the client-side to immediately apply Target offers
   const serverState = {
-    request: response.request,
-    response: response.response
+    request: targetResponse.request,
+    response: targetResponse.response
   };
 
+  // Build the final HTML page by replacing the Target config and state placeholders with appropriate values
   let result = TEMPLATE.replace(/\$\{organizationId\}/g, CONFIG.organizationId)
     .replace("${clientCode}", CONFIG.client)
-    .replace("${visitorState}", JSON.stringify(response.visitorState))
+    .replace("${visitorState}", JSON.stringify(targetResponse.visitorState))
     .replace("${serverState}", JSON.stringify(serverState, null, " "));
 
   if (CONFIG.serverDomain) {
@@ -41,10 +59,17 @@ function sendHtml(res, response) {
     result = result.replace('serverDomain: "${serverDomain}"', "");
   }
 
+  // Send the page to the client with a 200 OK HTTP status
   res.status(200).send(result);
 }
 
+/**
+ * Returns the regular React app to the client, without any Target state, in case there was an error
+ * with fetching Target data
+ * @param res response object returned to the client
+ */
 function sendErrorHtml(res) {
+  // Build the final HTML page, only Target config values are set in the template, for at.js initialization
   let result = TEMPLATE.replace(/\$\{organizationId\}/g, CONFIG.organizationId)
     .replace("${clientCode}", CONFIG.client)
     .replace("${visitorState}", "{}")
@@ -59,12 +84,17 @@ function sendErrorHtml(res) {
   res.status(200).send(result);
 }
 
-function sendResponse(res, response) {
+/**
+ *
+ * @param res response object returned to the client
+ * @param targetResponse response received from Target Delivery API
+ */
+function sendResponse(res, targetResponse) {
   res.set({ "Content-Type": "text/html" });
 
-  saveCookie(res, response.targetCookie);
-  saveCookie(res, response.targetLocationHintCookie);
-  sendHtml(res, response);
+  saveCookie(res, targetResponse.targetCookie);
+  saveCookie(res, targetResponse.targetLocationHintCookie);
+  sendHtml(res, targetResponse);
 }
 
 function sendErrorResponse(res) {
