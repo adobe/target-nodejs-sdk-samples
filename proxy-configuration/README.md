36c6921e6479202aad1ed437a104b8713fd939f7
ericfichtel <33603621+ericfichtel@users.noreply.github.com>
2023-02-14 09:24:01 -0800

    TNT-46700 Adding documentation and samples for proxy configuration (#53)
    * sample for adding a proxy to the node sdk http calls
    
    * Small typo fix
    
    Co-authored-by: Andrei Anischevici <andrei002@gmail.com>
    
    ---------
    
    Co-authored-by: Eric Fichtel <efichtel@adobe.com>
    Co-authored-by: Andrei Anischevici <andrei002@gmail.com>

---
+++ proxy-configuration/README.md
@@ -0,0 +1,21 @@
+# Proxy Configuration Sample
+
+## Overview
+
+It is possible to configure a proxy for the Node SDK's HTTP requests. This is possible by overriding the `fetch` API used by the SDK during initialization.
+
+This sample contains three different code snippets that demonstrate how to configure the SDK with a proxy, depending on which Node version the SDK runs on:  
+
+- [Node 18.2+](proxy-sample-node-18.js) (`undici.fetch` is the default `fetch`)
+- [Node 16.8+](proxy-sample-node-16.js) (`undici.fetch` needs to be imported)
+- [Node 14+](proxy-sample-node-14.js) (the proxy is configured with `node-fetch`)
+
+The difference in proxy configurations is due to differences in the fetch implementations between Node versions. 
+Starting with Node 18.2, the default implementation of `fetch` switched to `undici.fetch`. 
+However, `unidici.fetch` is only supported in Node 16.8+. Users are welcome to choose their own `fetch` and proxy client configurations. 
+These samples only serve as guidance for those considering adding a proxy to their SDK HTTP calls. 
+
+## Usage
+
+1. `npm start` to start a local proxy server
+2. in a separate window, run `node proxy-sample-node-[version]` to initialize a sample Target Client and make a `getOffers` call with a proxy configured
---
+++ proxy-configuration/proxy-sample-node-16.js
@@ -0,0 +1,69 @@
+// proxy example for Node 16.8+ users
+const TargetClient = require("@adobe/target-nodejs-sdk");
+const { ProxyAgent, fetch } = require("undici");
+
+let client;
+
+const context = {
+  channel: "web",
+  address: {
+    url: "http://local-target-test"
+  },
+  userAgent:
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0"
+};
+
+const visitorId = {
+  tntId: "338e3c1e51f7416a8e1ccba4f81acea0.28_0",
+  marketingCloudVisitorId: "07327024324407615852294135870030620007"
+};
+
+const targetRequest = {
+  id: visitorId,
+  context
+};
+
+function getOffers() {
+  client
+    .getOffers({
+      request: {
+        ...targetRequest,
+        prefetch: {
+          mboxes: [
+            {
+              name: "jason-flags",
+              index: 1
+            }
+          ]
+        }
+      },
+      sessionId: "dummy_session"
+    })
+    .then(res => {
+      console.log("Result: Success", JSON.stringify(res, null, 4));
+    })
+    .catch(err => {
+      console.log("ERROR");
+      console.log(err.message);
+    });
+}
+
+const proxyAgent = new ProxyAgent("http://localhost:8080");
+
+const fetchImpl = (url, options) => {
+  const fetchOptions = options;
+  fetchOptions.dispatcher = proxyAgent;
+  return fetch(url, fetchOptions);
+};
+
+client = TargetClient.create({
+  client: "targettesting",
+  fetchApi: fetchImpl,
+  organizationId: "74F652E95F1B16FE0A495C92@AdobeOrg",
+  decisioningMethod: "on-device",
+  events: { clientReady: getOffers },
+  logger: {
+    debug: (...messages) => console.log(...messages),
+    error: (...messages) => console.log(...messages)
+  }
+});
